d2eb2b45e9bf612b3b71e134b68c75b9bd5df274
Sebastiano Mariani <seba0691@gmail.com>
2015-04-12 13:31:34 +0200

    added socket.io client, problem on listening port
    Signed-off-by: Sebastiano Mariani <seba0691@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var app = require('express')();
 var router = express.Router();
+
+//load modules
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -8,6 +13,17 @@ router.get('/', function(req, res) {
 
 router.get('/chat', function(req, res) {
   res.render('chat');
+});
+
+
+/*---------------------- Socket.io events ----------------------*/
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
 
