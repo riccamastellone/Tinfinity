b69c5b4b4ee535583f6b76ee478337b704ae4cae
Sebastiano Mariani <seba0691@gmail.com>
2015-04-12 15:25:14 +0200

    send message to server working
    Signed-off-by: Sebastiano Mariani <seba0691@gmail.com>

--- custom_modules/io_module.js
+++ custom_modules/io_module.js
@@ -5,8 +5,18 @@ module.exports = function (io) {
 
     /*---------------------- Socket.io events ----------------------*/
 
+    /**
+    * Event emitted when the user connects
+    */
     io.on('connection', function(socket){
       console.log('a user connected');
+
+      //message passed
+      socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+      });
+
+
     });
 
 
