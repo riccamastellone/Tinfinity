994e78f6d02df860006a78ffe8ebeb5edd3fa5b3
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2015-05-10 16:41:05 +0200

    Nuovo endpoint con la lista delle conversazioni /api/chat

--- routes/chat.js
+++ routes/chat.js
@@ -7,6 +7,25 @@ var io = require('socket.io').listen(app.listen());
 // db.chat.insert({"user1" : "5530d2eb218629bd9fac825a", "user2" : "5530d2ec218629bd9fac825b", "data" : {"user1" : {"553cede16cfae4e7b96762a0" : ["MEss utente 1","1430056736956"],"553cede16cfae4e7b96762aa" : ["Altro mess utente2","1430056736960"]},"user2" : {"553cede16cfae4e7b96762f0" : ["Ciao questo è un messaggio","1430056736856"],"553cede16cfae4e7b96762fa" : ["Ciao questo è un secndo messaggio","1430056736959"]}}})
 
 /**
+ * /api/chat
+ * Recupero l'elenco delle conversazioni dell'utente loggato
+ * returns { .. }
+ */
+router.get('/', function (req, res, next) {
+  var db = req.db;
+  var chat = db.get('chat');
+
+  // Non abbiamo la certezza di chi sia utente1 e utente2,
+  // quindi cerchiamo entrambe le combinazioni
+  chat.find({ $or: [ 
+  	{'user1': User._id.toString() },
+  	{'user2': User._id.toString() },
+  	]}).sort({lastModifiedDate:-1}, function(err, doc){
+    	res.json(doc);
+  });
+});
+
+/**
  * /api/chat/{id}
  * Recupero lo storico di una conversazione 
  * con l'utente {id}
