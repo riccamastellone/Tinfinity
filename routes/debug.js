e0750834338c57f7505762974d4677fba37b66d7
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2015-08-29 10:41:15 +0200

    Debug Chat

--- routes/debug.js
+++ routes/debug.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-
-router.get('/user1', function(req, res) {
-  res.render('chat', {'user1' : '5530d2eb218629bd9fac825a', 'user2' : '5530d2ec218629bd9fac825b', 'token' : 'thisisatoken'});
-});
-router.get('/user2', function(req, res) {
-  res.render('chat', {'user2' : '5530d2eb218629bd9fac825a', 'user1' : '5530d2ec218629bd9fac825b', 'token' : 'thisisanothertoken'});
+// Lets use the chat online!
+router.get('/webchat', function(req, res) {
+	token = prompt('Give me your token');
+	user1 = prompt('Give me your user id');
+	user2 = prompt('Give me your friend user id');
+  	res.render('chat', {'user1' : user1, 'user2' : user2, 'token' : token});
 });
 
 module.exports = router;
