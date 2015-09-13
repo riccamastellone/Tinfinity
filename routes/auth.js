29fc80177d3d3b8fe5479a32b296d06b6221bfaf
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2015-09-13 14:56:33 +0200

    request

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
-
+var request = require("request");
 /**
  * /api/auth/fb
  * Autentico il client. Nel body della post mi aspetto il token
@@ -32,43 +32,42 @@ router.post('/fb', function(req, res, next) {
 		    // Sia che esista, che non, creiamo un nuovo token di accesso
 	    	var token = require('crypto').randomBytes(32).toString('hex');
 
-	    	
-          	// Recuperiamo l'immagine così da averla in locale e servirla in base64
-          	https.get(fb_data.picture.data.url, function(obj) {
-          		var body = '';
+	    	// Recuperiamo l'immagine così da averla in locale e servirla in base64
+	    	request({
+		      uri: url,
+		      encoding: 'binary'
+		    }, function(error, response, body) {
+		      var image;
+		      if (!error && response.statusCode === 200) {
+		        image = new Buffer(body.toString(), "binary").toString("base64");
+		        // E aggiorniamo tutti i dati presi da facebook
+		    	var user = {
+	                name : fb_data.first_name,
+	                surname : fb_data.last_name,
+	                gender : fb_data.gender,
+	                facebook_id : fb_data.id,
+	                token : token,
+	                images: {
+	                	'0' : image
+	                },
+	                updated_at : Date.now()
+	          	}
 
-			    obj.on('data', function(chunk) {
-			        body += chunk;
-			    });
-		 		obj.on('end', function () {
-          			var base64image = new Buffer(body).toString('base64');
+	          	users.update({ facebook_id : fb_data.id},
+					{ $set : user, $setOnInsert: { created_at: Date.now() } },
+					{ upsert: true }, function(err, doc){
+						if(err) throw err;
+						// Pare la callback dell'update non ritorni l'oggetto, 
+						// quindi lo andiamo a ripescare così
+						users.findOne({ facebook_id : fb_data.id},function(err,doc){
+							res.json(doc);
+						});
+				});
+		      }
+		    });
 
-
-          			// E aggiorniamo tutti i dati presi da facebook
-			    	var user = {
-		                name : fb_data.first_name,
-		                surname : fb_data.last_name,
-		                gender : fb_data.gender,
-		                facebook_id : fb_data.id,
-		                token : token,
-		                images: {
-		                	'0' : base64image
-		                },
-		                updated_at : Date.now()
-		          	}
-
-		          	users.update({ facebook_id : fb_data.id},
-						{ $set : user, $setOnInsert: { created_at: Date.now() } },
-						{ upsert: true }, function(err, doc){
-							if(err) throw err;
-							// Pare la callback dell'update non ritorni l'oggetto, 
-							// quindi lo andiamo a ripescare così
-							users.findOne({ facebook_id : fb_data.id},function(err,doc){
-								res.json(doc);
-							});
-					});
-		        });
-          	});
+          	
+          	
 
 		  });
 	}).on('error', function(e) {
